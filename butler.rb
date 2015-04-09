9af37bee46d7426e2fae0185c9f7388355c16e4c
Michael Allen <michael@michaelallen.io>
2015-04-09 13:32:05 +0100

    wrong sha

--- butler.rb
+++ butler.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Butler < Formula
   homepage 'https://github.com/michaeldfallen/butler'
   url 'https://github.com/michaeldfallen/butler/archive/0.2.tar.gz'
-  sha1 'b997c07c03588e7e2cbd30f4eb1c6a0af004da41'
+  sha1 '04bbcfe6df0a6ef7ed5baf9a451d545277930891'
 
   head 'https://github.com/michaeldfallen/butler.git'
 
