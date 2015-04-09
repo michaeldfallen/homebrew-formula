159b2fba6b0ef2219adcc69af567ab702959cc35
Michael Allen <michael@michaelallen.io>
2015-04-09 14:49:03 +0100

    bump to v0.3

--- butler.rb
+++ butler.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Butler < Formula
   homepage 'https://github.com/michaeldfallen/butler'
-  url 'https://github.com/michaeldfallen/butler/archive/0.2.tar.gz'
+  url 'https://github.com/michaeldfallen/butler/archive/0.3.tar.gz'
   sha1 '04bbcfe6df0a6ef7ed5baf9a451d545277930891'
 
   head 'https://github.com/michaeldfallen/butler.git'
