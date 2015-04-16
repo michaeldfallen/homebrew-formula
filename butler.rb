270a0884f20970bdca971da953dce405cb768ed3
Michael Allen <michael@michaelallen.io>
2015-04-16 15:45:15 +0100

    bump butler to 0.5

--- butler.rb
+++ butler.rb
@@ -2,14 +2,17 @@ require 'formula'
 
 class Butler < Formula
   homepage 'https://github.com/michaeldfallen/butler'
-  url 'https://github.com/michaeldfallen/butler/archive/0.4.tar.gz'
-  sha1 '5e91699d4396b864146ac4e3a49be93930663065'
+  url 'https://github.com/michaeldfallen/butler/archive/0.5.tar.gz'
+  sha1 '58ff9c44d5b6faef7128aff5b2804418bca85c05'
 
   head 'https://github.com/michaeldfallen/butler.git'
 
   def install
     libexec.install 'butler'
     libexec.install 'butler-base.sh'
+    libexec.install 'complete.zsh'
+    libexec.install 'complete.bash'
+    libexec.install 'init.sh'
     bin.install_symlink libexec/'butler'
   end
 end
