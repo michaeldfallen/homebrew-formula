2e583ba5882a9475ee17ff58defb2e3b4e3348ec
Michael Allen <michael@michaelallen.io>
2015-04-22 16:40:31 +0100

    bump butler to version 0.6

--- butler.rb
+++ butler.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Butler < Formula
   homepage 'https://github.com/michaeldfallen/butler'
-  url 'https://github.com/michaeldfallen/butler/archive/0.5.tar.gz'
-  sha1 '58ff9c44d5b6faef7128aff5b2804418bca85c05'
+  url 'https://bintray.com/artifact/download/michaelallen/homebrew-releases/butler-0.6.tar.gz'
+  sha1 '5189bfa0d46a99c3ea9b18ee509dfadcc8b9004f'
 
   head 'https://github.com/michaeldfallen/butler.git'
 
@@ -17,8 +17,12 @@ class Butler < Formula
     libexec.install 'init.sh'
     bin.install_symlink libexec/'butler'
 
-    ohai "To enable completion add:"
-    ohai "  source \"$(butler --init-completion)\""
-    ohai "To your ~/.bashrc or ~/.zshrc"
+    ohai "Bash and Zsh completion
+
+To enable completion add:
+
+    source \"$(butler --init-completion)\"
+
+To your ~/.bashrc or ~/.zshrc"
   end
 end
