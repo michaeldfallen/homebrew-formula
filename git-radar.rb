15bd3df4113556cf723f9a2640784923832e886e
Michael Allen <michael@michaelallen.io>
2015-08-25 17:35:28 +0100

    Deploy git-radar via brew

---
+++ git-radar.rb
@@ -0,0 +1,28 @@
+require 'formula'
+
+# Homebrew formula that installs git-radar
+class GitRadar < Formula
+  homepage 'https://github.com/michaeldfallen/git-radar'
+  url 'https://bintray.com/artifact/download/michaelallen/homebrew-releases/git-radar-0.1.tar.gz'
+  sha1 'eb44245f4349c1f585b5101fae06a5baedb655b0'
+
+  head 'https://github.com/michaeldfallen/git-radar.git'
+
+  def install
+    libexec.install 'git-radar'
+    libexec.install 'radar-base.sh'
+    libexec.install 'prompt.zsh'
+    libexec.install 'prompt.bash'
+    libexec.install 'fetch.sh'
+    bin.install_symlink libexec/'git-radar'
+
+    ohai "Bash and Zsh installation
+
+Bash
+  export PS1=\"\W\$(git-radar --bash --fetch) \"
+
+Zsh
+  export PROMPT=\"%1/%\$(git-radar --zsh --fetch) \"
+"
+  end
+end
