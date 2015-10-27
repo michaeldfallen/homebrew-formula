266e2ad69b9a0d33fed4ed882bb0744314e750ed
Michael Allen <michael@michaelallen.io>
2015-10-27 11:22:32 +0000

    Release v0.5 of git-radar

--- git-radar.rb
+++ git-radar.rb
@@ -3,8 +3,8 @@ require 'formula'
 # Homebrew formula that installs git-radar
 class GitRadar < Formula
   homepage 'https://github.com/michaeldfallen/git-radar'
-  url 'https://github.com/michaeldfallen/git-radar/releases/download/v0.4/git-radar-0.4.tar.gz'
-  sha1 '51d21cada554b3a8a6384ecfbc96b3138a5443d7'
+  url 'https://github.com/michaeldfallen/git-radar/releases/download/v0.5/git-radar-0.5.tar.gz'
+  sha1 '629bf547fda1e9fa54332998169c011040a179df'
 
   head 'https://github.com/michaeldfallen/git-radar.git'
 
